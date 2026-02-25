9af12aaa073fa5376e34585795272914bc1049f1
Supakit <supakit.kitj@gmail.com>
2026-02-25 20:10:48 +0700

    fix: code format

--- src/algorithms/utils.ts
+++ src/algorithms/utils.ts
@@ -1,10 +1,8 @@
-
 export function defaultCompare<T>(a: T, b: T): number {
   if (a < b) return -1;
   if (a > b) return 1;
   return 0;
 }
-
 
 export function validateArray<T>(arr: T[], name = 'array'): T[] {
   if (!Array.isArray(arr)) {
