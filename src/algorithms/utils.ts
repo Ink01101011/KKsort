8eb63380794ca735ccaf09087aa25838cb2bff9f
Supakit <supakit.kitj@gmail.com>
2026-02-25 21:16:03 +0700

    fix: Fix lint error

--- src/algorithms/utils.ts
+++ src/algorithms/utils.ts
@@ -1,5 +1,4 @@
-export function defaultCompare<T>(a: T, b: T): number;
-export function defaultCompare(a: any, b: any): number {
+export function defaultCompare<T>(a: T, b: T): number {
   if (a < b) return -1;
   if (a > b) return 1;
   return 0;
