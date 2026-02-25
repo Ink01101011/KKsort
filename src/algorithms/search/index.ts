6bdb2f16995fbf15a63d90a44234165619ba4afb
Supakit <supakit.kitj@gmail.com>
2026-02-25 18:10:04 +0700

    fix: Fix pre check

--- src/algorithms/search/index.ts
+++ src/algorithms/search/index.ts
@@ -1,4 +1,10 @@
-export { default as binarySearch, default as binarySearchDefault } from './binary';
-export { default as linearSearch, default as linearSearchDefault } from './linear';
+export {
+  default as binarySearch,
+  default as binarySearchDefault,
+} from './binary';
+export {
+  default as linearSearch,
+  default as linearSearchDefault,
+} from './linear';
 export { default as jumpSearch, default as jumpSearchDefault } from './jump';
 export { default as quickSearch, default as quickSearchDefault } from './quick';
